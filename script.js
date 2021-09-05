b82433319feb39cc0a1545dc8a23a0bf586af968
jerrysdevs <87643563+jerrysdevs@users.noreply.github.com>
2021-09-06 00:36:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,57 +79,30 @@ contentdiv2.append(audio)
 parentcontentdiv.append(contentdiv)
 parentcontentdiv1.append(contentdiv1)
 parentcontentdiv2.append(contentdiv2)
-cdiv2.append(parentcontentdiv)
-cdiv2.append(parentcontentdiv1)
-cdiv2.append(parentcontentdiv2)
+cdiv2.append(parentcontentdiv,parentcontentdiv1,parentcontentdiv2)
 cdiv1.append(cdiv2)
 pdiv.append(cdiv1)
 
 
 document.body.append(pdiv)
 
-// async function getmean() {
-//     var uri = (`https://api.dictionaryapi.dev/api/v2/entries/en/`)
-//     let key = await document.getElementById("in1").value;
-//     let keyword = await key.replaceAll(/\s/g, '')
-//     console.log(keyword)
-//     let data = await fetch(`${uri}${keyword}`)
-//     let datajson = await data.json()
-//     document.getElementById('p1').innerHTML =
-//         "<p><b>Definition:</b>  " + datajson[0].meanings[0].definitions[0].definition + "</br></p>" +
-//         "<p><b>Example: </b>  " + datajson[0].meanings[0].definitions[0].example + "</br></p>" +
-//         "<p><b>synonyms: </b> " + datajson[0].meanings[0].definitions[0].synonyms[0] + "</br></p>" +
-//         "<p><b>antonyms: </b> " + datajson[0].meanings[0].definitions[0].antonyms[0] + "</br></p>";
-//     var link = datajson[0].phonetics[0].audio
-//     console.log(link)
-//     document.getElementById('audio').innerHTML = `<audio controls autoplay src=${link}> </audio>`
-// }
-
-
-
-
-
+//api docs link https://openexchangerates.org/
 async function request() {
-    let url = "http://api.currencylayer.com/live?access_key=69ee96f9416d691c3b8425931e0c9809&currencies="
+    let url = "https://openexchangerates.org/api/latest.json?app_id=8b9acf150bf546e1afa3e576038fc38e&symbols="
     let input1 = document.getElementById("in1").value;
     console.log(input1)
     let input2 = document.getElementById("in2").value;
     let input3 = document.getElementById("in3").value;
-    console.log(`${url}${input1},${input2}`)
-    let req = await fetch(`${url}${input1},${input2}`)
-    console.log(req['quotes'])
-    let response = await req.json()
-    // console.log(response.quotes.USDAED)
-    let value1 = `USD${input1}`.toUpperCase()
-    console.log(value1)
-    let value2 = `USD${input2}`.toUpperCase()
-    let restxt = `${input2}`.toUpperCase()
-    console.log(response.quotes[value1])
-    let result = response.quotes[value1]/ response.quotes[value2]
-    console.log(result)
-    let result1 = input3/result
-    console.log(result1)
-    audio.innerHTML = `${restxt} ${result1}`
+    console.log(`${url}${input1},${input2}`);
+    let req = await fetch(`${url}${input1},${input2}`);
+    let response = await req.json();
+    let value1 = `${input1}`.toUpperCase();
+    let value2 = `${input2}`.toUpperCase();
+    let restxt = `${input2}`.toUpperCase();
+    var result = await response.rates[value1]/ response.rates[value2];
+    let result1 = await input3/result;
+    audio.innerHTML = `${restxt}  ${result1}`;
 }
+
 
 
